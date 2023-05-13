2c8465b38f34f671433ccbbb00accdcee38363dd
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-05-13 20:41:07 +0530

    terraform-practice-data-sources

--- roboshop-terraform/servers.tf
+++ roboshop-terraform/servers.tf
@@ -1,6 +1,16 @@
+variable "instance-type" {
+  default="t3.micro"
+}
+
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
 resource "aws_instance" "frontend" {
   ami = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
+  instance_type = var.instance-type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = { Name = "frontend" }
 }
+
