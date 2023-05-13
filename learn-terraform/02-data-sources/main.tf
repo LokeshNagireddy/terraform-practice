54b1b62b074c5b9bb441a3fc288b9af21b9f9cfd
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-05-13 20:32:46 +0530

    terraform-practice-data-sources

--- learn-terraform/02-data-sources/main.tf
+++ learn-terraform/02-data-sources/main.tf
@@ -2,6 +2,18 @@ data "aws_security_group" "first" {
   name = "allow-all"
 }
 
-output "security-group-id"{
+output "security-group"{
   value=data.aws_security_group.first
+}
+
+output "security-group-id"{
+  value=data.aws_security_group.first.id
+}
+
+output "security-group-vpc-id"{
+  value=data.aws_security_group.first.vpc_id
+}
+
+output "security-group-arn"{
+  value=data.aws_security_group.first.arn
 }
