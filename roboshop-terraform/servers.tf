4d95e2baee0a0704c2c351106bcbfdb478c7b6a8
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-05-14 20:27:20 +0530

    terraform-practice-loops

--- roboshop-terraform/servers.tf
+++ roboshop-terraform/servers.tf
@@ -1,16 +1,31 @@
+/*
 variable "instance-type" {
   default="t3.micro"
 }
+*/
 
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
-resource "aws_instance" "frontend" {
+variable "resources" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    mongodb ={
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+  }
+}
+resource "aws_instance" "instance" {
+  for_each = var.resources
   ami = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance-type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
-  tags = { Name = "frontend" }
+  tags = { Name = each.value["name"] }
 }
 
