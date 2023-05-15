567decafada03b0c600c40308ca5985f4c42f203
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-05-15 11:19:17 +0530

    terraform-practice-state

--- roboshop-terraform/state.tf
+++ roboshop-terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-practice-robo"
-    key    = "roboshop-terraform/terraform.tfstate"
+    key    = "roboshop-terraform/dev/terraform.tfstate"
     region ="us-east-1"
   }
 }
